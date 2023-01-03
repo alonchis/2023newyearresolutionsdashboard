c53e7b34db56b5d01ffd9740150ca63ba60ea5d3
alonso <alonso.yo2@gmail.com>
2023-01-03 01:14:00 -0800

    initial working state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
+class Item extends React.Component {
+    render() {
+        return (
+                <li><input type="checkbox"/></li>
+        );
+    }
+}
+
+function List(props) {
+    const numbers = props.numbers;
+    const listItems = numbers.map((number) =>
+        <Item></Item>
+    );
+    return (
+        <ul>{listItems}</ul>
+    );
+}
+
 function App() {
+    const numbers = [1, 2, 3, 4, 5]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*<header className="App-header">*/}
+      {/*  <img src={logo} className="App-logo" alt="logo" />*/}
+      {/*  /!*<p>*!/*/}
+      {/*  /!*  Edit <code>src/App.js</code> and save to reload.*!/*/}
+      {/*  /!*</p>*!/*/}
+      {/*</header>*/}
+        <div className="Item" class="container">
+
+            <List title="test tile" numbers={numbers}/>
+            {/*{List numbers={numbers}}*/}
+        </div>
     </div>
   );
 }
