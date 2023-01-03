b8034c32eb09efe4a34fb5997c50645bc1f89274
alonso <alonso.yo2@gmail.com>
2023-01-03 02:03:12 -0800

    text written but needs alignment. added final count of checkboxes but still needs work

--- src/App.js
+++ src/App.js
@@ -5,24 +5,40 @@ import './App.css';
 class Item extends React.Component {
     render() {
         return (
-                <li><input type="checkbox"/></li>
+                <dd><input type="checkbox"/></dd>
         );
     }
 }
 
-function List(props) {
+function List(props)  {
     const numbers = props.numbers;
     const listItems = numbers.map((number) =>
         <Item></Item>
     );
     return (
-        <ul>{listItems}</ul>
+        <div class="center">
+            <dl>
+                <div><dt>{props.title}</dt></div>
+                <div>{listItems}</div>
+            </dl>
+        </div>
     );
+
 }
 
 function App() {
     const numbers = [1, 2, 3, 4, 5]
+    const numbers2 = [550, 600, 650]
+    const numbers3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    const numbers4 = [295, 290, 285, 280, 275]
+    const numbers5 = [500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000, 5500, 6000, 6500, 7000, 7500, 8000, 8500, 9000, 9500, 10000]
+    const numbers6 = [1, 2, 3]
+    const numbers7 = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
 
+    const numbers8 = []
+    for (let i = 1; i <= 52; i++) {
+        numbers8.push(i)
+    }
   return (
     <div className="App">
       {/*<header className="App-header">*/}
@@ -32,10 +48,27 @@ function App() {
       {/*  /!*</p>*!/*/}
       {/*</header>*/}
         <div className="Item" class="container">
-
-            <List title="test tile" numbers={numbers}/>
-            {/*{List numbers={numbers}}*/}
+            <List title="Write, edit, or delete 10k lines of code" numbers={numbers2}/>
         </div>
+        <div className="Item" className="container">
+            <List title="650 meet PR" numbers={numbers3}/>
+        </div>
+        <div className="Item" className="container">
+            <List title="Read one book a month" numbers={numbers4}/>
+        </div>
+        <div className="Item" className="container">
+            <List title="Body weight of 275 lbs" numbers={numbers5}/>
+        </div>
+        <div className="Item" className="container">
+            <List title="Get Certified" numbers={numbers6}/>
+        </div>
+        <div className="Item" className="container">
+            <List title="Invest 500k" numbers={numbers7}/>
+        </div>
+        <div className="Item" className="container">
+            <List title="watch more Movies" numbers={numbers8}/>
+        </div>
+
     </div>
   );
 }
